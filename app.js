f9ec633a74f5e7b98a320ce18f57392694fc8573
Swarup <swarupkum45@gmail.com>
2022-12-19 01:20:24 +0530

    mongo start

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const { urlencoded } = require("body-parser");
 
 
@@ -12,6 +13,15 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res)=>{
     res.render("home");
@@ -23,6 +33,22 @@ app.get("/login", (req, res)=>{
 
 app.get("/register", (req, res)=>{
     res.render("register");
+});
+
+app.post("/register", (req, res)=>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save((err)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("secrets");
+        }
+    });
 });
 
 app.listen(3000,()=>{
